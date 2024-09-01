c4f0504058a1db8955c483889c34fb8bf42c5eed
Akash Kennedy <akashkennedy1@gmail.com>
2024-09-01 11:53:57 +0530

    Created Brands Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import logo1 from "/public/logo1.png";
+import logo2 from "/public/logo2.png";
+import logo3 from "/public/logo3.png";
+import logo4 from "/public/logo4.png";
+import logo5 from "/public/logo5.png";
 
 export default function Home() {
   return (
@@ -53,6 +58,13 @@ export default function Home() {
           height={999}
           width={999}
         />
+      </section>
+      <section className="min-h-28 bg-black w-full flex justify-evenly">
+        <Image src={logo1} alt="logo1" className="h-auto w-auto py-10" />
+        <Image src={logo2} alt="logo2" className="h-auto w-auto py-10" />
+        <Image src={logo3} alt="logo3" className="h-auto w-auto py-10" />
+        <Image src={logo4} alt="logo4" className="h-auto w-auto py-10" />
+        <Image src={logo5} alt="logo5" className="h-auto w-auto py-10" />
       </section>
     </main>
   );
